38380169f0ee79ba13a34f7746401a14cad1db29
emilygknight <emilyknight819@gmail.com>
2023-12-20 22:26:42 -0600

    fixed files to work

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const database = await openDB('jate', 1);
+  const transaction = database.transaction('jate', 'readwrite');
+  const store = transaction.objectStore('jate');
+  const request = store.put({  id: 1, value: content });
+  const result = await request;
+  console.log(result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const database = await openDB('jate', 1);
+  const transaction = database.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log(result);
+  if (result) {
+    console.log("data recieved")
+    return result.value
+  } else {
+    console.log("no data found")
+  }
+};
 
 initdb();
